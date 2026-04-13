779fe8f3306531d353f3ea166f5315f870d1732c
treeswift <treeswift@mailbox.org>
2026-04-12 23:08:08 -0500

    2D histogram of a layer

--- straightface.cpp
+++ straightface.cpp
@@ -23,20 +23,35 @@ int main(int argc, char **argv) {
     const char* file_name = argv[1];
     cv::Mat src = cv::imread(file_name);   
 
-    std::vector<cv::Mat> trg(6);
+    const int thumb_layer = 5;  // -> config
+    std::vector<cv::Mat> trg(thumb_layer + 1);
 #if SF_SEPARATE_CHANNELS
     trg.at(0) = src;
 #else
     cv::cvtColor(src, trg.at(0), cv::COLOR_BGR2HSV);
 #endif
-    skim(trg.at(1), trg.at(0));
-    skim(trg.at(2), trg.at(1));
-    skim(trg.at(3), trg.at(2));
-    skim(trg.at(4), trg.at(3));
-    skim(trg.at(5), trg.at(4));
+    for(int layer_id = 1; layer_id <= thumb_layer; ++layer_id) {
+        skim(trg.at(layer_id), trg.at(layer_id-1));
+    }
+
+    const int max_hue = 180;
+    const int max_val = 256;
+    const int histogram_dims = 2;
+    const int histogram_dim[] = {max_hue, max_val};
+    const float color_range[] = {0, max_hue};
+    const float light_range[] = {0, max_val};
+    const float* ranges[] = {color_range, light_range};
+    const int hsv_components[] = {0, 2};
+
+    auto& layer = trg.at(thumb_layer);
+
+    cv::Mat hist_hl;
+    cv::calcHist(&layer, 1, hsv_components, cv::Mat(), hist_hl, histogram_dims, histogram_dim, ranges);
+    assert(hist_hl.rows == max_hue);
+    assert(hist_hl.cols == max_val);
 
     ui::Frame frame("sample display");
-    frame.display(trg.at(5));
+    frame.display(layer);
     frame.waitSingle();
 
     return 0;
