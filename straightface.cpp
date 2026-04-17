1c9a63b6849f3b07f53b0c0cdd0631212b83bffc
treeswift <treeswift@mailbox.org>
2026-04-17 05:21:39 -0500

    WIP quadrangle fitting (function compiles)

--- straightface.cpp
+++ straightface.cpp
@@ -1,18 +1,40 @@
 #include "windowframe.h"
 
+#include "opencv2/core/optim.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 
 #include <stdio.h>  // I prefer fprintf to stream "<<" syntax
 
+#include <cmath>
 #include <utility>
 #include <vector>
+
+// extract to: geometry.h
+inline cv::Point2d rot90(const cv::Point2d& vec) { return {vec.y, -vec.x}; }
+double dot_p(const cv::Point2d& v1, const cv::Point2d& v2) { return v1.x * v2.x + v1.y * v2.x; }
+double rot_p(const cv::Point2d& v1, const cv::Point2d& v2) { return dot_p(v1, rot90(v2)); }
 
 // struct AppModel{}
 
 void skim(cv::Mat& trg, const cv::Mat& src) {
     cv::resize(src, trg, cv::Size{}, 0.5, 0.5, cv::INTER_LINEAR);
 }
+
+struct LambdaFunction : public cv::MinProblemSolver::Function {
+    LambdaFunction(int dims, std::function<double(const double*)> expr) : dimensions(dims), impl(expr) {}
+
+    double calc(const double* x) const override {
+        return impl(x);
+    }
+
+    int getDims() const override {
+        return dimensions;
+    }
+
+    int dimensions;
+    std::function<double(const double*)> impl;
+};
 
 // TODO expose the remaining value of `shrink_samples`
 std::pair<int, int> shrink_linear(const cv::Mat& hist, float shrink_samples) {
@@ -39,7 +61,6 @@ void dump_range(cv::Mat mat, int i, int j) {
     for(; i < j; ++i) {
         fprintf(stderr, "Bin %03d: %2.3f\n", i, mat.at<Pixel>(i));
     }
-
 }
 
 template<typename Pixel>
@@ -111,7 +132,74 @@ int main(int argc, char **argv) {
     // here comes the model
     cv::Point2i anchor = {-1, -1};
 
+    cv::Scalar fillco = {150, 0, 255};
+    cv::Scalar lineco = {30, 128, 255};
+
     auto lines = trg.at(lines_layer).clone();
+
+    std::function<double(const double*)> fitness = [&](const double* octopus) {
+        const cv::Point2d* tetrapod = reinterpret_cast<const cv::Point2d*>(octopus);
+        // testing for boundaries (TODO make compact):
+        for(int i = 0; i < 4; ++i) {
+            const cv::Point2d vert = tetrapod[i];
+            if(vert.x < 0. || vert.y < 0. || vert.x >= lines.cols || vert.y >= lines.rows) {
+                return 0.;
+            }
+        }
+
+        // testing for convexity (TODO merge s0..s3 calculation with previous):
+        cv::Point2d s0 = tetrapod[0] - tetrapod[1];
+        cv::Point2d s1 = tetrapod[1] - tetrapod[2];
+        cv::Point2d s2 = tetrapod[2] - tetrapod[3];
+        cv::Point2d s3 = tetrapod[3] - tetrapod[0];
+
+        // we want all the angles to have the same CW/CCW direction,
+        // i.e. all the side "rot products" at the angles to have the same sign
+        // (the sign inversion due to direction inversion is factored out by the comparison)
+        bool a0 = std::signbit(rot_p(s0, s1));
+        bool a1 = std::signbit(rot_p(s1, s2));
+        bool a2 = std::signbit(rot_p(s2, s3));
+        bool a3 = std::signbit(rot_p(s3, s0));
+        if((a0 != a1) || (a2 != a3) || (a0 != a2)) {
+            return 0.; // the quadrangle is self-intersecting or concave
+        }
+
+        cv::Point2d d0 = tetrapod[0] - tetrapod[2];
+        cv::Point2d d1 = tetrapod[1] - tetrapod[3];
+        // turn d1 by 90 degrees:
+        //  cv::Point2d r1 = {d1.y, -d1.x};
+        //  area == |d0| * |d1| * sin(d0, d1) == |d0| * |r1| * cos(d0, r1) == dot(d0, r1)
+        // then, inlining:
+        double area = abs(rot_p(d0, d1));
+
+        auto isat = [&](const cv::Point2d& pt) { return lines.at<cv::Scalar>((int) pt.x, (int) pt.y) == fillco; };
+        auto elen = [&](int side_index) {
+            const cv::Point2d& v0 = tetrapod[side_index];
+            const cv::Point2d& v1 = tetrapod[(side_index + 1) % 4];
+            const cv::Point2d side = v1 - v0; // TOOD edges[side_index] // TODO rename sides into edges
+
+            double elen = sqrt(v1.x * v1.x + v1.y * v1.y);
+            if(elen < 1.) {
+                return (int) isat(v0);
+            }
+            cv::Point2d step = side / elen;
+            int ilen = (int) elen;
+            int imin = std::numeric_limits<int>::max();
+            int imax = 0;
+            for(int i = 0; i < ilen; ++i) {
+                bool atis = isat(v0 + step * i); // TODO change to search from both ends, slightly more optimal
+                if(atis) {
+                    imin = std::min(imin, i);
+                    imax = std::max(imax, i);
+                }
+            }
+            return (imin <= imax) ? (imax - imin + 1) : 0;
+        };
+
+        double edge = elen(0) + elen(1) + elen(2) + elen(3);
+        return 0. - area * edge;
+    };
+
     frame.display(lines); // intermediate LoD
     int greed = frame.addKnob("greed", [&](int val){
         greed = val;
@@ -124,8 +212,11 @@ int main(int argc, char **argv) {
             float tolerance = expf(0.1 * greed);  // MOREINFO consider int16 or fp32 pyramid?
             // fprintf(stderr, "%d, %d tolerance=%f\n", x, y, tolerance);
             cv::Scalar tol3 = {tolerance, tolerance, tolerance};
-            cv::floodFill(lines, anchor, {150, 0, 255}, nullptr, tol3, tol3, 0 * cv::FLOODFILL_FIXED_RANGE);
+            cv::floodFill(lines, anchor, fillco, nullptr, tol3, tol3, 0 * cv::FLOODFILL_FIXED_RANGE);
             // TODO use overload to update selection mask; suggest a contrasting color/choose from a palette
+            
+            // beginning with the seed point, write the solver:
+            ;;
         }
         frame.display(lines); // refresh
         return false;
